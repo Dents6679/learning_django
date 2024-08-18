9d4d49410a1126dec8daeace336b51e6702cea82
Tom Dent <tomdenty1@gmail.com>
2024-08-18 13:01:53 +0100

    Changed navbar button to direct to user profile and added an "edit profile" button on profiles.

--- users/models.py
+++ users/models.py
@@ -9,7 +9,7 @@ import os
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-    bio = models.TextField(default='Hello! I am a new user.')
+    bio = models.TextField(default='Hello! I am a new user.', max_length=200)
 
     def __str__(self):
         return f'{self.user.username}\'s Profile'
